bb4720ffe137e4e7c839f7e6d92afcc90b4e5d1c
Estelle Foo <estellefoo@Estelles-MacBook-Pro.local>
2026-09-14 16:41:20 -0400

    Finished Exercise 4

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,9 +1,20 @@
-import boto3
-
+import json
+import glob
+import csv
+from pathlib import Path
+import pandas as pd
 
 def main():
     # your code here
-    pass
+    data = Path('data')
+    for path in data.rglob('*.json'):
+        if path.is_file():
+            with open(path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                df = pd.json_normalize(data)
+                df.to_csv(str(path).replace('json', 'csv'), index=False)
+
+
 
 
 if __name__ == "__main__":
