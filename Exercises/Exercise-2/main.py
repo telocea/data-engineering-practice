5f8959aea04561beeeef1e1e9e908905f3078a29
Estelle <ff1estelle@gmail.com>
2026-09-14 15:09:00 -0400

    Exercise 1 & 2

--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,11 +1,45 @@
 import requests
-import pandas
-
+import pandas as pd
+from bs4 import BeautifulSoup
 
 def main():
     # your code here
-    pass
+    source = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
+    # find file
+    r = requests.get(source)
+    if r.status_code == 200:
+        # read file
+        content = r.content
+        soup = BeautifulSoup(content, 'html.parser')
 
+        td_list = soup.find_all('td', string='2024-01-19 15:27')
+
+        # load file into pandas
+        all_highest = pd.DataFrame()
+        for td in td_list:
+            uri = source + td.find_previous_sibling('td').a.get('href')    
+            df = pd.read_csv(uri).dropna(subset=['HourlyDryBulbTemperature'])
+            df.loc[:, 'HourlyDryBulbTemperature'] = df['HourlyDryBulbTemperature'].apply(lambda x: extractNums(x))
+            highest = df[df['HourlyDryBulbTemperature'] == df['HourlyDryBulbTemperature'].max()]
+            all_highest = pd.concat([all_highest, highest], ignore_index=True)
+        # find record(s) with highest 'HourlyDryBulbTemperature'
+        max = all_highest[all_highest['HourlyDryBulbTemperature'] == all_highest['HourlyDryBulbTemperature'].max()]
+        print(max)
+    else:
+        print('Invalid URI')
+
+def extractNums(s):
+    if isinstance(s, float) or isinstance(s, int):
+        return s
+    else:
+        result = ''
+        for i in s:
+            if i.isnumeric() or i == '.':
+                result += i
+        if result:
+            return float(result)
+        else:
+            return float('-inf')
 
 if __name__ == "__main__":
     main()
--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -2,6 +2,17 @@ import boto3
 
 
 def main():
+    # download file
+    s3 = boto3.client('s3')
+    bucket = 'commoncrawl'
+    key = 'crawl-data/CC-MAIN-2022-05/wet.paths.gz'
+
+    s3.download_file(bucket, key, 'wet.paths.gz')
+    # extract file
+
+    # download and store file
+
+    # print each line of the file
     # your code here
     pass
 
